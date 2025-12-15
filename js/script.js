a300345a3420df11a5d52d059a234f4155b19e2f
23CABVIJAYADHARSHINIK <139529852+23CABVIJAYADHARSHINIK@users.noreply.github.com>
2025-12-15 18:46:26 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,76 @@
+const cards = document.querySelectorAll(".card");
+//console.log(cards);
+let matchCard = 0;
+let cardOne, cardTwo;
+let disableDeck = false
+function flipCard(e) {
+    let clickedCard = e.target; //getting user clicked card
+    if(clickedCard !== cardOne && !disableDeck){
+        clickedCard.classList.add("flip");
+        if(!cardOne)
+        {
+            //reture the cardOne value to clickedCard
+            return cardOne = clickedCard;
+        }
+        cardTwo =clickedCard;
+        disableDeck = true;
+        let cardOneImg = cardOne.querySelector("img").src;
+        let cardTwoImg = cardTwo.querySelector("img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+function matchCards(img1, img2)
+{
+    if(img1 === img2)
+    {
+        // if two cards img matched
+        matchCard++; // increment matched value by 1
+        //if matched value is 8 that user has matched all the cards {8*2 =16 cards}
+        if(matchCard == 8)
+        {
+            setTimeout(() => {
+                return shuffleCard();
+            }, 1000); //calling shuffleCard function after 1 sec
+        }
+        cardOne.removeEventListener("click", flipCard);
+        cardTwo.removeEventListener("click", flipCard);
+        cardOne = cardTwo = ""; //setting both card value to blank
+        return disableDeck = false;
+    }
+    
+    // if two cards not matched
+    setTimeout(() => {
+        //adding shake class to both card after 400ms
+        cardOne.classList.add("shake");
+        cardTwo.classList.add("shake");
+    }, 400);
+
+    setTimeout(() => {
+        //removing boyh shake & flip class from both card after 1.2s
+        cardOne.classList.remove("shake", "flip");
+        cardTwo.classList.remove("shake", "flip");
+        cardOne = cardTwo = ""; //setting both card value to blank
+        disableDeck = false;
+    }, 1200);
+}
+function shuffleCard(){
+    matchCard = 0;
+    cardOne = cardTwo = "";
+    disableDeck = false;
+    //creating array of 16 items is repeated twice
+    let arr = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
+    arr.sort(() => Math.random() > 0.5 ? 1 : -1); //sorting array item randomly random passing  image to each card
+
+    //removing flip class from all cards
+    cards.forEach((card, index) => {
+        card.classList.remove("flip");
+        let imgTag = card.querySelector("img");
+        imgTag.src= `./images/img-${arr[index]}.png`
+        card.addEventListener("click", flipCard);
+    });
+}
+shuffleCard();
+
+cards.forEach(card => {//adding click event to all cards
+    card.addEventListener("click", flipCard);
+});
